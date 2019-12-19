e86ffe56377d0e08fc4ce6b68d228545d1208c85
Daniel Franklin <dfranklinsmail@gmail.com>
2019-12-18 20:51:57 -0500

    add darts project to my git with some changes

--- cnn/test.py
+++ cnn/test.py
@@ -16,21 +16,24 @@ from torch.autograd import Variable
 from model import NetworkCIFAR as Network
 
 
-parser = argparse.ArgumentParser("cifar")
+parser = argparse.ArgumentParser()
+#parser = argparse.ArgumentParser("cifar")
 parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=96, help='batch size')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
 parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
 parser.add_argument('--layers', type=int, default=20, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='EXP/model.pt', help='path of pretrained model')
+parser.add_argument('--model_path', type=str, default='cifar10_model.pt', help='path of pretrained model')
+#parser.add_argument('--model_path', type=str, default='EXP/model.pt', help='path of pretrained model')
 parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
 parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
-args = parser.parse_args()
+args = parser.parse_known_args()[0]
+#args = parser.parse_args()
 
 log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
@@ -40,6 +43,7 @@ CIFAR_CLASSES = 10
 
 
 def main():
+  print('hello world')
   if not torch.cuda.is_available():
     logging.info('no gpu device available')
     sys.exit(1)
